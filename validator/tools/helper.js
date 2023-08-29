f509b335e10101f5c6e89ec540fed0245391d23e
Adrian Polo <a.whole.dev@gmail.com>
2023-08-29 16:04:50 +0200

    add a message

--- validator/tools/helper.js
+++ validator/tools/helper.js
@@ -1,1 +1,61 @@
-console.log(process.env)
+const { Octokit } = require("@octokit/rest");
+
+class GithubHelper {
+
+    static GH_PR_EVENT_APPROVE = "APPROVE";
+    static GH_PR_EVENT_REQUEST_CHANGES = "REQUEST_CHANGES";
+    static GH_PR_EVENT_COMMENT = "COMMENT";
+    static GH_PR_EVENT_PENDING = "PENDING";
+
+    static VALID_GH_PR_EVENTS = [this.GH_PR_EVENT_APPROVE, this.GH_PR_EVENT_COMMENT, this.GH_PR_EVENT_PENDING, this.GH_PR_EVENT_REQUEST_CHANGES]
+
+    constructor(token){
+        this.githubOwner = null;
+        if(process.env.GITHUB_OWNER) {
+            this.githubOwner = process.env.GITHUB_OWNER;
+        }
+        this.githubRepository = null;
+        if(process.env.GITHUB_REPOSITORY) {
+            this.githubRepository = process.env.GITHUB_REPOSITORY;
+        }
+        this.prNumber = null;
+        if(process.env.PR_NUMBER) {
+            this.prNumber = process.env.PR_NUMBER;
+        }
+        this.octokit = null;
+        if (process.env.GITHUB_TOKEN) {
+            let githubToken = process.env.GITHUB_TOKEN;
+            this.octokit = new OctoKit({
+                auth: githubToken
+            });
+        }
+        this.enabled = true;
+        if(this.githubRepository == null || this.prNumber == null || this.octokit == null) {
+            this.enabled = false;
+        }
+    }
+
+    async createReviewPR(body, event) {
+        let prEvent = event
+        if(!VALID_GH_PR_EVENTS.includes(event)) {
+            prEvent = GH_PR_EVENT_PENDING;
+        }
+        try {
+
+            let reviewPrRequest = {
+                owner: this.githubOwner,
+                repo: this.githubRepository,
+                pull_number: this.prNumber,
+                body: body,
+                event: prEvent
+            }
+
+            await this.octokit.pulls.createReviewComment(reviewPrRequest);
+        } catch (error) {
+            console.error("Error to create the message:", error);
+        }
+    }
+
+}
+
+export default GithubHelper();
